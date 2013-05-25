e0d9cd9426e7ab9a880fedb037d6dc49194cfddc
wooque <wooque@gmail.com>
2013-05-25 23:11:04 +0200

    Some changes in Node to support progressive expanding

--- student/Node.java
+++ student/Node.java
@@ -1,27 +1,19 @@
 package student;
 
-import java.util.LinkedList;
+import java.util.*;
 import reversi.*;
 
 public class Node {
 
     private Board board;
-    private Position move;
     private LinkedList<Node> children;
-    private Node next;
+    private List<Position> moves;
+    private Position move;
 
     public Node(Board board, Position move) {
         this.board = board;
         this.move = move;
         children = new LinkedList<>();
-    }
-
-    public Node getNext() {
-        return next;
-    }
-
-    public void setNext(Node next) {
-        this.next = next;
     }
 
     public Board getBoard() {
@@ -31,16 +23,20 @@ public class Node {
     public LinkedList<Node> getChildren() {
         return children;
     }
-
-    public void addChildren(Board board, Position move) {
-        children.add(new Node(board, move));
+    
+    public List<Position> getMoves() {
+        return moves;
     }
     
-    public void clearChildren(){
-        children.clear();
+    public void setMoves(List<Position> moves) {
+        this.moves = moves;
     }
-
+    
     public Position getMove() {
         return move;
+    }  
+    
+    public void addChildren(Node child) {
+        children.add(child);
     }
 }
